51c6651842aa62b70f0c66ba6daf8261796b8cf7
Julio Berina <julioberina@gmail.com>
2020-10-17 21:03:04 -0700

    Add basic functionality for navbar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,12 +9,19 @@ import { faBars, faArrowLeft } from '@fortawesome/free-solid-svg-icons';
 export class AppComponent {
   public faBars = faBars;
   public faArrowLeft = faArrowLeft;
+  public arrowClass = 'hidden';
+
+  private pageStack = [];
 
   public menuClicked() {
     console.log('menu clicked');
+    this.pageStack.push('component');
+    this.arrowClass = '';
   }
 
   public arrowClicked() {
     console.log('arrow clicked');
+    this.pageStack.pop();
+    this.arrowClass = this.pageStack.length === 0 ? 'hidden' : '';
   }
 }
